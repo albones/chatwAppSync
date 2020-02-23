551891f91f6fcb09886ecf2a9d0c92cfc7a1ffb6
albones <33490799+albones@users.noreply.github.com>
2020-02-23 11:56:18 -0700

    built files

---
+++ build/precache-manifest.9cf753bb709a2a8c1f90d67aa4bc914d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5323267f0c4961e5e97a7c38c1ac4fea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e606c75ac12f5adfe704",
+    "url": "/static/css/2.6bf18bf3.chunk.css"
+  },
+  {
+    "revision": "5934850207454535db2c",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e606c75ac12f5adfe704",
+    "url": "/static/js/2.0820a193.chunk.js"
+  },
+  {
+    "revision": "b278cd47c093f5772b870eb4174adf52",
+    "url": "/static/js/2.0820a193.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5934850207454535db2c",
+    "url": "/static/js/main.3cca5742.chunk.js"
+  },
+  {
+    "revision": "09cd4e33a876a871720e",
+    "url": "/static/js/runtime-main.ed1676f7.js"
+  }
+]);
